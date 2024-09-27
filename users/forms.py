1fd26cac39e9a8225ad89f8f7650f901de8f81c3
user <danilchumak80@gmail.com>
2024-09-27 16:00:55 +0300

    Добавил формы

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.forms import HiddenInput, ModelForm
 
 from users.models import User
 
@@ -6,4 +7,20 @@ from users.models import User
 class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ("email", "password1", "password2")
+        fields = ["email", "password1", "password2"]
+
+
+class UserProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ["email", "first_name", "last_name", "phone", "avatar", "company"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["password"].widget = HiddenInput()
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ["is_active"]
