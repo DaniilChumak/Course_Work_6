6b459cd617b014516863ccbdf0cecac8d6f24820
user <danilchumak80@gmail.com>
2024-09-27 15:58:58 +0300

    Убрано дублирование подключение моделей в админке

--- users/admin.py
+++ users/admin.py
@@ -1,17 +1,7 @@
 from django.contrib import admin
 
-from emailservice.models import Mailing, Client
+from users.models import User
 
 
-@admin.register(Mailing)
-class MailingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'created_at', 'period_mail', 'start_mail', 'stop_mail', 'owner')
-    list_filter = ('owner',)
-    search_fields = ('name', 'owner')
-
-
-@admin.register(Client)
-class ClientAdmin(admin.ModelAdmin):
-    list_display = ('email', 'name', 'last_name', 'owner')
-    list_filter = ('owner',)
-    search_fields = ('email', 'owner')
+# Register your models here.
+admin.site.register(User)
